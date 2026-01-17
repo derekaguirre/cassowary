e4b9cfeb13263fad2ea37db77fb0781059500b85
derekaguirre <derekaguirre@me.com>
2026-01-17 14:29:09 -0700

    Added up to 3.0 boss drops and local specialties and reworded the welcome page

--- app/components/home/Welcome.vue
+++ app/components/home/Welcome.vue
@@ -1,15 +1,14 @@
 <template>
-	<section class="flex flex-wrap gap-5">
-		<div>
-			<p class="">
-				Cassowary is a planner for Wuthering Waves, a open-world action RPG game from Kuro Games. This tool
-				helps you to plan and track on levelling characters or weapons, and also manage your inventory. As for now, this
-				still has a lot of work to do but it's usable in some way. Also now it has notes.
-			</p>
-
-		</div>
-	</section>
+  <section class="flex flex-wrap gap-5">
+    <div>
+      <p class="">
+        Cassowary is a planner for Wuthering Waves, an open-world action RPG
+        from Kuro Games. This tool helps you to plan and track characters'
+        levels and weapons, or also manage your inventory. Stay tuned for future
+        updates.
+      </p>
+    </div>
+  </section>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
