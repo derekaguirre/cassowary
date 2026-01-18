718fc74d405c87f57cd7bb963d24791316e0b34b
derekaguirre <derekaguirre@me.com>
2026-01-17 22:12:18 -0700

    Update formatting

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
-import { defineVitestConfig } from '@nuxt/test-utils/config';
+import { defineVitestConfig } from "@nuxt/test-utils/config";
 
 export default defineVitestConfig({
-	// any custom Vitest config you require
-	test: {
-		environment: 'nuxt',
-	},
+  // any custom Vitest config you require
+
+  test: {
+    environment: "nuxt",
+  },
 });
